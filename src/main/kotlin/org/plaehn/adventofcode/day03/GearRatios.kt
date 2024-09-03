af70380da018deb361f1f0d1273dcfeb05711c87
Oliver Plaehn <Plaehn.Oliver@swm.de>
2024-09-03 12:55:24 +0200

    Day 3: Collect part numbers

--- src/main/kotlin/org/plaehn/adventofcode/day03/GearRatios.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day03/GearRatios.kt
@@ -3,14 +3,37 @@ package org.plaehn.adventofcode.day03
 import org.plaehn.adventofcode.common.Matrix
 
 
-class GearRatios(val engineSchematic: Matrix<Char>) {
+class GearRatios(private val engineSchematic: Matrix<Char>) {
 
     fun computeSumOfAllPartNumbers(): Long {
-        // TODO
+        val partNumbers = buildList {
+            engineSchematic.rows().forEachIndexed { rowIndex, row ->
+                digitRegex
+                    .findAll(row.joinToString(""))
+                    .forEach { matchResult ->
+                        add(
+                            PartNumber(
+                                partNumber = matchResult.value.toInt(),
+                                row = rowIndex,
+                                columnRange = matchResult.range
+                            )
+                        )
+                    }
+            }
+        }
+        println(partNumbers)
         return 0
     }
 
+    data class PartNumber(
+        val partNumber: Int,
+        val row: Int,
+        val columnRange: IntRange
+    )
+
     companion object {
+
+        private val digitRegex = "\\d+".toRegex()
 
         fun fromInput(input: List<String>) =
             GearRatios(Matrix.fromRows(input.map { line -> line.toList() }, '.'))
