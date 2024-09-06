6e18605186267cb3de800c2850a740f3da07489b
“plaehn” <oliver.plaehn@gmail.com>
2024-09-07 00:23:21 +0200

    Day 6: Solution for part 1

--- src/main/kotlin/org/plaehn/adventofcode/day06/WaitForIt.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day06/WaitForIt.kt
@@ -1,19 +1,25 @@
 package org.plaehn.adventofcode.day06
 
+import org.plaehn.adventofcode.common.product
 import org.plaehn.adventofcode.common.tokenize
 
 class WaitForIt(private val races: List<Race>) {
 
-    fun computeProductOfNumberOfWaysToBeatRecord(): Int {
-        println(races)
-        return 0
-    }
+    fun computeProductOfNumberOfWaysToWin(): Int =
+        races
+            .map { it.computeNumberOfWaysToWin() }
+            .product()
 
 
     data class Race(
         val duration: Int,
         val recordDistance: Int
-    )
+    ) {
+        fun computeNumberOfWaysToWin(): Int =
+            (1..<duration)
+                .map { buttonDuration -> (duration - buttonDuration) * buttonDuration }
+                .count { distance -> distance > recordDistance }
+    }
 
     companion object {
         fun fromInput(lines: List<String>): WaitForIt {
--- src/test/kotlin/org/plaehn/adventofcode/day06/WaitForItTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day06/WaitForItTest.kt
@@ -8,25 +8,25 @@ import org.plaehn.adventofcode.common.readLines
 class WaitForItTest {
 
     @Test
-    fun `Compute product of number of ways to beat record for test input`() {
+    fun `Compute product of number of ways to win for test input`() {
         val lines = this::class.java.readLines("test_input.txt")
 
         val waitForIt = WaitForIt.fromInput(lines)
 
-        val product = waitForIt.computeProductOfNumberOfWaysToBeatRecord()
+        val product = waitForIt.computeProductOfNumberOfWaysToWin()
 
         assertThat(product).isEqualTo(288)
     }
 
     @Test
-    fun `Compute product of number of ways to beat record for puzzle input`() {
+    fun `Compute product of number of ways to win for puzzle input`() {
         val lines = this::class.java.readLines("puzzle_input.txt")
 
         val waitForIt = WaitForIt.fromInput(lines)
 
-        val product = waitForIt.computeProductOfNumberOfWaysToBeatRecord()
+        val product = waitForIt.computeProductOfNumberOfWaysToWin()
 
-        assertThat(product).isEqualTo(-1)
+        assertThat(product).isEqualTo(227850)
     }
 }
 
