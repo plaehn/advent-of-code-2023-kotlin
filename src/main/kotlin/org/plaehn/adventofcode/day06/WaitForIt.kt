b871c2692694b26e113a23ad2666a098b0c0bd0e
“plaehn” <oliver.plaehn@gmail.com>
2024-09-07 00:34:49 +0200

    Day 6: Solution for part 2

--- src/main/kotlin/org/plaehn/adventofcode/day06/WaitForIt.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day06/WaitForIt.kt
@@ -5,27 +5,35 @@ import org.plaehn.adventofcode.common.tokenize
 
 class WaitForIt(private val races: List<Race>) {
 
-    fun computeProductOfNumberOfWaysToWin(): Int =
+    fun computeProductOfNumberOfWaysToWin(): Long =
         races
             .map { it.computeNumberOfWaysToWin() }
             .product()
 
 
     data class Race(
-        val duration: Int,
-        val recordDistance: Int
+        val duration: Long,
+        val recordDistance: Long
     ) {
-        fun computeNumberOfWaysToWin(): Int =
+        fun computeNumberOfWaysToWin(): Long =
             (1..<duration)
                 .map { buttonDuration -> (duration - buttonDuration) * buttonDuration }
                 .count { distance -> distance > recordDistance }
+                .toLong()
     }
 
     companion object {
-        fun fromInput(lines: List<String>): WaitForIt {
-            val durations = lines.first().split(":").last().tokenize().map { it.toInt() }
-            val distances = lines.last().split(":").last().tokenize().map { it.toInt() }
-            val races = durations.zip(distances).map { (duration, distance) -> Race(duration, distance) }
+        fun fromInput(lines: List<String>, singleRace: Boolean = false): WaitForIt {
+            val durations = lines.first().split(":").last().tokenize().map { it.toLong() }
+            val distances = lines.last().split(":").last().tokenize().map { it.toLong() }
+
+            val races = if (singleRace) {
+                val singleDuration = durations.joinToString("") { it.toString() }.toLong()
+                val singleDistance = distances.joinToString("") { it.toString() }.toLong()
+                listOf(Race(singleDuration, singleDistance))
+            } else {
+                durations.zip(distances).map { (duration, distance) -> Race(duration, distance) }
+            }
 
             return WaitForIt(races)
         }
--- src/test/kotlin/org/plaehn/adventofcode/day06/WaitForItTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day06/WaitForItTest.kt
@@ -15,7 +15,7 @@ class WaitForItTest {
 
         val product = waitForIt.computeProductOfNumberOfWaysToWin()
 
-        assertThat(product).isEqualTo(288)
+        assertThat(product).isEqualTo(288L)
     }
 
     @Test
@@ -26,7 +26,29 @@ class WaitForItTest {
 
         val product = waitForIt.computeProductOfNumberOfWaysToWin()
 
-        assertThat(product).isEqualTo(227850)
+        assertThat(product).isEqualTo(227850L)
+    }
+
+    @Test
+    fun `Compute product of number of ways to win single race for test input`() {
+        val lines = this::class.java.readLines("test_input.txt")
+
+        val waitForIt = WaitForIt.fromInput(lines, singleRace = true)
+
+        val product = waitForIt.computeProductOfNumberOfWaysToWin()
+
+        assertThat(product).isEqualTo(71503L)
+    }
+
+    @Test
+    fun `Compute product of number of ways to win single race for puzzle input`() {
+        val lines = this::class.java.readLines("puzzle_input.txt")
+
+        val waitForIt = WaitForIt.fromInput(lines, singleRace = true)
+
+        val product = waitForIt.computeProductOfNumberOfWaysToWin()
+
+        assertThat(product).isEqualTo(42948149L)
     }
 }
 
