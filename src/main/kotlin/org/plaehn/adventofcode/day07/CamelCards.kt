769aa664f8fbaef8e98a4559ac04c99167d0d92a
“plaehn” <oliver.plaehn@gmail.com>
2024-09-07 15:22:40 +0200

    Day 7: Solution part 2

--- src/main/kotlin/org/plaehn/adventofcode/day07/CamelCards.kt
+++ src/main/kotlin/org/plaehn/adventofcode/day07/CamelCards.kt
@@ -1,19 +1,20 @@
 package org.plaehn.adventofcode.day07
 
 import org.plaehn.adventofcode.common.tokenize
+import org.plaehn.adventofcode.day07.CamelCards.Card.JOKER
 import org.plaehn.adventofcode.day07.CamelCards.Type.*
 
 class CamelCards(val handsWithBids: List<HandWithBid>) {
 
     fun computeTotalWinnings(): Long =
         handsWithBids
-            .sortedBy { it.hand }
+            .sortedBy { it.hand }.also { println(it.joinToString("\n")) }
             .mapIndexed { index, handWithBid -> (index + 1) * handWithBid.bid }
             .sum()
 
     companion object {
-        fun fromInput(lines: List<String>) =
-            CamelCards(handsWithBids = lines.map { HandWithBid.fromInput(it) })
+        fun fromInput(lines: List<String>, withJokers: Boolean = false) =
+            CamelCards(handsWithBids = lines.map { HandWithBid.fromInput(it, withJokers) })
     }
 
     data class HandWithBid(
@@ -21,10 +22,10 @@ class CamelCards(val handsWithBids: List<HandWithBid>) {
         val bid: Long
     ) {
         companion object {
-            fun fromInput(input: String): HandWithBid {
+            fun fromInput(input: String, withJokers: Boolean): HandWithBid {
                 val (hand, bid) = input.tokenize()
                 return HandWithBid(
-                    hand = Hand.fromInput(hand),
+                    hand = Hand.fromInput(hand, withJokers),
                     bid = bid.toLong()
                 )
             }
@@ -32,21 +33,74 @@ class CamelCards(val handsWithBids: List<HandWithBid>) {
     }
 
     data class Hand(
-        val cards: List<Card>
+        val cards: List<Card>,
+        val withJokers: Boolean
     ) : Comparable<Hand> {
 
         private val type: Type = determineType()
 
         private fun determineType(): Type {
-            val sameCardCounts = cards.groupingBy { it }.eachCount().values.sortedDescending()
+            val cardWithCount: List<Pair<Card, Int>> =
+                cards.groupingBy { it }.eachCount().entries.map { it.key to it.value }.sortedByDescending { it.second }
             return when {
-                sameCardCounts.size == 1 -> FIVE_OF_A_KIND
-                sameCardCounts.size == 2 && sameCardCounts[0] == 4 -> FOUR_OF_A_KIND
-                sameCardCounts.size == 2 && sameCardCounts[0] == 3 -> FULL_HOUSE
-                sameCardCounts.size == 3 && sameCardCounts[0] == 3 -> THREE_OF_A_KIND
-                sameCardCounts.size == 3 -> TWO_PAIR
-                sameCardCounts.size == 4 -> ONE_PAIR
-                else -> HIGH_CARD
+                // 5
+                cardWithCount.size == 1 -> FIVE_OF_A_KIND
+
+                // 4, 1
+                cardWithCount.size == 2 && cardWithCount[0].second == 4 -> {
+                    if (cardWithCount[0].first == JOKER || cardWithCount[1].first == JOKER) {
+                        FIVE_OF_A_KIND
+                    } else {
+                        FOUR_OF_A_KIND
+                    }
+                }
+
+                // 3, 2
+                cardWithCount.size == 2 && cardWithCount[0].second == 3 -> {
+                    if (cardWithCount[0].first == JOKER || cardWithCount[1].first == JOKER) {
+                        FIVE_OF_A_KIND
+                    } else {
+                        FULL_HOUSE
+                    }
+                }
+
+                // 3, 1, 1
+                cardWithCount.size == 3 && cardWithCount[0].second == 3 -> {
+                    if (cardWithCount.any { it.first == JOKER }) {
+                        FOUR_OF_A_KIND
+                    } else {
+                        THREE_OF_A_KIND
+                    }
+                }
+
+                // 2, 2, 1
+                cardWithCount.size == 3 -> {
+                    if (cardWithCount[0].first == JOKER || cardWithCount[1].first == JOKER) {
+                        FOUR_OF_A_KIND
+                    } else if (cardWithCount[2].first == JOKER) {
+                        FULL_HOUSE
+                    } else {
+                        TWO_PAIR
+                    }
+                }
+
+                // 2, 1, 1, 1
+                cardWithCount.size == 4 -> {
+                    if (cardWithCount.any { it.first == JOKER }) {
+                        THREE_OF_A_KIND
+                    } else {
+                        ONE_PAIR
+                    }
+                }
+
+                // 1, 1, 1, 1, 1
+                else -> {
+                    if (cardWithCount.any { it.first == JOKER }) {
+                        ONE_PAIR
+                    } else {
+                        HIGH_CARD
+                    }
+                }
             }
         }
 
@@ -64,8 +118,11 @@ class CamelCards(val handsWithBids: List<HandWithBid>) {
                 .firstOrNull { it != 0 } ?: 0
 
         companion object {
-            fun fromInput(input: String) =
-                Hand(cards = input.map { Card.fromChar(it) })
+            fun fromInput(input: String, withJokers: Boolean) =
+                Hand(
+                    cards = input.map { Card.fromChar(it, withJokers) },
+                    withJokers = withJokers
+                )
         }
     }
 
@@ -80,6 +137,7 @@ class CamelCards(val handsWithBids: List<HandWithBid>) {
     }
 
     enum class Card(val chr: Char) {
+        JOKER('J'),
         TWO('2'),
         THREE('3'),
         FOUR('4'),
@@ -95,10 +153,14 @@ class CamelCards(val handsWithBids: List<HandWithBid>) {
         ACE('A');
 
         companion object {
-            private val chr2Card = entries.associateBy { it.chr }
+            private val chr2Card = entries.drop(1).associateBy { it.chr }
 
-            fun fromChar(chr: Char): Card =
-                chr2Card[chr] ?: throw IllegalArgumentException("Unknown card $chr")
+            fun fromChar(chr: Char, withJokers: Boolean): Card =
+                if (withJokers && chr == 'J') {
+                    JOKER
+                } else {
+                    chr2Card[chr] ?: throw IllegalArgumentException("Unknown card $chr")
+                }
         }
     }
 }
--- src/test/kotlin/org/plaehn/adventofcode/day07/CamelCardsTest.kt
+++ src/test/kotlin/org/plaehn/adventofcode/day07/CamelCardsTest.kt
@@ -26,6 +26,28 @@ class CamelCardsTest {
 
         val totalWinnings = camelCards.computeTotalWinnings()
 
-        assertThat(totalWinnings).isEqualTo(246424613)
+        assertThat(totalWinnings).isEqualTo(246424613L)
+    }
+
+    @Test
+    fun `Compute total winnings with jokers for test input`() {
+        val lines = this::class.java.readLines("test_input.txt")
+
+        val camelCards = CamelCards.fromInput(lines, withJokers = true)
+
+        val totalWinnings = camelCards.computeTotalWinnings()
+
+        assertThat(totalWinnings).isEqualTo(5905L)
+    }
+
+    @Test
+    fun `Compute total winnings with jokers for puzzle input`() {
+        val lines = this::class.java.readLines("puzzle_input.txt")
+
+        val camelCards = CamelCards.fromInput(lines, withJokers = true)
+
+        val totalWinnings = camelCards.computeTotalWinnings()
+
+        assertThat(totalWinnings).isEqualTo(248256639)
     }
 }
